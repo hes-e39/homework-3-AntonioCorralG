438e565e713648c2997b7f14e82b445627ea7e27
gcorral <gonzalo_corral@intuit.com>
2024-09-27 11:10:42 -0700

    changing package url

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 const App = () => {
+    const ColorButton = () => {
+        return <button>Color me!</button>;
+    };
     return (
         <>
             <h1>Welcome to Color World!</h1>
+            <ColorButton />
         </>
     );
 };
